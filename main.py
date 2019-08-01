f257c621089de5cc827a8362205f90ad97599bd4
pick4er <pick4er@gmail.com>
2019-08-01 15:16:03 +0300

    Connect bot to hardcoded chat_id

--- main.py
+++ main.py
@@ -8,26 +8,25 @@ sslify = SSLify(app)
 
 TOKEN = os.environ['TOKEN']
 API_URL = f'https://api.telegram.org/bot{TOKEN}/'
+CHAT_ID=-1001435572035
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
   if request.method == 'POST':
     parsed_request = request.get_json()
 
-    message = parsed_request['message']
-    chat_id = message['chat']['id']
-    message_text = message['text']
-    send_message(chat_id, message_text)
+    order = parsed_request['order']
+    send_message(order)
 
     return jsonify(parsed_request)
 
   return '<h1>Hello, bookstore bot!</h1>'
 
-def send_message(chat_id, message=''):
+def send_message(message=''):
   url = API_URL + 'sendMessage'
   body = {
-    'chat_id': chat_id,
-    'text': message,
+    'chat_id': CHAT_ID,
+    'text': message
   }
   return requests.post(url, json=body)
 
